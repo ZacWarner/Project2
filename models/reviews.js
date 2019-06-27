648242d4a3d5f12c6d1ec0bc5e331e47670becee
Patrick Owens <manbearpat@gmail.com>
2019-06-26 18:23:41 -0700

    formatting changes

--- models/reviews.js
+++ models/reviews.js
@@ -1,29 +1,31 @@
-module.exports = function (sequelize, DataTypes) {
-    var Reviews = sequelize.define("Reviews", {
-        reviewer_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        review: {
-            type: DataTypes.TEXT,
-        },
-        product_id: {
-            type: DataTypes.INTEGER,
-        }
+module.exports = function(sequelize, DataTypes) {
+  var Reviews = sequelize.define("Reviews", {
+    // eslint-disable-next-line camelcase
+    reviewer_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    review: {
+      type: DataTypes.TEXT
+    },
+    // eslint-disable-next-line camelcase
+    product_id: {
+      type: DataTypes.INTEGER
+    }
+  });
+
+  Reviews.associate = function(models) {
+    // We're saying that a Post should belong to an Author
+    // A Post can't be created without an Author due to the foreign key constraint
+    Reviews.belongsTo(models.Login, {
+      foreignKey: {
+        allowNull: false
+      }
     });
+  };
 
-    Reviews.associate = function (models) {
-        // We're saying that a Post should belong to an Author
-        // A Post can't be created without an Author due to the foreign key constraint
-        Reviews.belongsTo(models.Login, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-    };
-
-    return Reviews;
+  return Reviews;
 };
