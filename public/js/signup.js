b5c8882bba32582d2374327b2bec8b61ff3bbb0c
ZacWarner <zektgn@gmail.com>
2019-06-28 23:25:49 -0700

    signup form now takes email and password and creates a user.

---
+++ public/js/signup.js
@@ -0,0 +1,43 @@
+
+
+//java for signup page.
+
+$(document).ready(function () {
+    var signForm = $("#newUsr");
+    var email = $("input#inputEmail4");
+    var password = $("input#inputPassword4");
+
+    signForm.on("submit", function (event) {
+        event.preventDefault();
+        var newUsr = {
+            username: email.val().trim(),
+            password: password.val().trim()
+        };
+
+        //makes sure we got the email and password
+        if (!newUsr.username || !newUsr.password) {
+            return;
+        }
+
+        createNewUsr(newUsr.username, newUsr.password);
+        email.val("");
+        password.val("");
+
+    });
+
+    function createNewUsr(email, password) {
+        $.post("/api/signup", {
+            username: email,
+            password: password
+        }).then(function (data) {
+            console.log("signed up");
+        }).catch(handleLoginError);
+    };
+
+    function handleLoginError(err) {
+        $("#alert .msg").text(err.responseJSON);
+        $("#alert").fadeIn(500);
+    };
+
+
+});
