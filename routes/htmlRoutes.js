8832c2c4d50dc224748a55e41f27b6a13405ebaa
Patrick Owens <manbearpat@gmail.com>
2019-07-01 21:37:45 -0700

    product page updates

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,37 +1,37 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     let you = req.user;
-  
+
     res.render("index", { user: you })
   });
   // Load Signup page
-  app.get("/signup", function(req, res)  {
-    
-      res.render("signup");
-    });
+  app.get("/signup", function (req, res) {
+
+    res.render("signup");
+  });
 
   // Load Login page
-  app.get("/login", function(req, res) {
+  app.get("/login", function (req, res) {
     res.render("login");
   });
   // Load searchSeller page
-  app.get("/searchSeller", function(req, res) {
+  app.get("/searchSeller", function (req, res) {
     res.render("searchSeller");
   });
   // Load products page
-  app.get("/products", function(req, res) {
+  app.get("/products", function (req, res) {
     res.render("products");
   });
 
   // Load products page and pass in a table by id
-  app.get("/project_2.products/:id", function(req, res) {
+  app.get("/project_2.products/:id", function (req, res) {
     db.project_2.products
       .findOne({ where: { id: req.params.id } })
       // eslint-disable-next-line camelcase
-      .then(function(dbproject_2) {
+      .then(function (dbproject_2) {
         res.render("project_2.products", {
           // eslint-disable-next-line camelcase
           products: dbproject_2.products
@@ -40,7 +40,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
