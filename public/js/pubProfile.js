b27d15b19e12a1bb943f4c92d5966d35395009d5
MeenaKrs <46361811+MeenaKrs@users.noreply.github.com>
2019-07-02 15:58:48 -0700

    PubProfile handlebars and js

---
+++ public/js/pubProfile.js
@@ -0,0 +1,31 @@
+/* eslint-disable indent */
+/* eslint-disable prettier/prettier */
+$.ajax({
+    url: "api/sellers/2",
+    type: "GET"
+}).then(
+    function (data) {
+        console.log(data);
+        $("#sellerPh").append(data.phone);
+        $("#sellerEmail").append(data.email);
+        $("#sellerLoc").append(data.location);
+    }
+);
+
+$.ajax({
+    url: "/api/products/seller/2",
+    type: "GET"
+}).then(
+    function (data) {
+        console.log(data);
+        for (i in data) {
+            console.log(data[i].picture);
+            var products = $("<img>");
+            products.attr("src", data[i].picture);
+            products.attr("width", "200");
+            products.attr("height", "200");
+            products.addClass("m-1")
+            $(".product-images").append(products);
+        }
+    }
+);
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,30 +1,34 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     res.render("index");
   });
   // Load Signup page
-  app.get("/signup", function(req, res) {
+  app.get("/signup", function (req, res) {
     res.render("signup");
+  });
+  // Load Search page
+  app.get("/search", function (req, res) {
+    res.render("pubProfile");
   });
 
   // Load Login page
-  app.get("/login", function(req, res) {
+  app.get("/login", function (req, res) {
     res.render("login");
   });
   // Load searchSeller page
-  app.get("/searchSeller", function(req, res) {
+  app.get("/searchSeller", function (req, res) {
     res.render("searchSeller");
   });
 
   // Load products page and pass in a table by id
-  app.get("/project_2.products/:id", function(req, res) {
+  app.get("/project_2.products/:id", function (req, res) {
     db.project_2.products
       .findOne({ where: { id: req.params.id } })
       // eslint-disable-next-line camelcase
-      .then(function(dbproject_2) {
+      .then(function (dbproject_2) {
         res.render("project_2.products", {
           // eslint-disable-next-line camelcase
           products: dbproject_2.products
@@ -33,7 +37,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
