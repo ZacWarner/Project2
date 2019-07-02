e50008518475b8dced6840f32b90a84dbe451c9b
ZacWarner <zektgn@gmail.com>
2019-07-01 19:30:37 -0700

    fixed the stat button so it's now taking

--- public/js/signup.js
+++ public/js/signup.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
     var name = $("input#inputName");
     var phone = $("input#inputPhone");
     var city = $("input#inputCity");
-    var state = $("input#inputState");
+    var state = $("#inputState");
 
     var stateList = ["AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MH", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "PR", "PW", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VI", "VT", "WA", "WI", "WV", "WY"];
 
