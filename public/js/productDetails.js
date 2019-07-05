15f3f5d205581be6ec6c1251aaf4b204f7536b30
MeenaKrs <46361811+MeenaKrs@users.noreply.github.com>
2019-07-04 21:33:57 -0700

    reviews to work

--- public/js/productDetails.js
+++ public/js/productDetails.js
@@ -3,7 +3,8 @@
 $(document).ready(function () {
     var sellerEmailId = "";
     console.log("Execution");
-    let dataAttr = $("#trackseller").attr("data-sellerid");
+    let dataAttr = $("#tracker").attr("data-sellerid");
+    let prodId = $("#tracker").attr("data-prodid");
     console.log("Seller id: " + dataAttr);
 
     $.get("/api/sellers/id/" + dataAttr, function (data) {
@@ -14,6 +15,29 @@ $(document).ready(function () {
         $("#modalname").append(data.name);
     });
 
+    $.get("/api/reviews/product/" + prodId, function (data) {
+        console.log("Reviews");
+        console.log(data);
+
+        for (i in data) {
+            let reviewcard = $("<div>");
+            reviewcard.addClass("card product-card m-1");
+            reviewcard.attr("data-reviewid", data[i].id);
+
+            let title = $("<div>");
+            title.html("<strong><i>" + data[i].reviewer_name + "</i></strong>");
+            title.addClass("card-title mx-3");
+
+            let review = $("<div>");
+            review.html(data[i].review);
+            review.addClass("card-body");
+
+            reviewcard.append(title, review);
+
+            $("#review-div").append(reviewcard);
+        }
+
+    });
 
     $("#dropEmail").on("click", function (event) {
         event.preventDefault();
