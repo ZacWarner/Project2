019928dd0c9435b02137415fcfa707aba8191048
MeenaKrs <46361811+MeenaKrs@users.noreply.github.com>
2019-07-05 19:31:38 -0700

    Change in category list

--- public/js/products.js
+++ public/js/products.js
@@ -1,38 +1,6 @@
 /* eslint-disable indent */
 /* eslint-disable prettier/prettier */
 
-var categoriesDistinct = [];
-
-// To filter distinct values from an array of elements
-function onlyUnique(value, index, self) {
-    return self.indexOf(value) === index;
-}
-
-// Load categories into drop down options
-function populateCategory() {
-    console.log("populating");
-    for (let i = 0; i < categoriesDistinct.length; i++) {
-        var newOption = $("<option>");
-        newOption.text(categoriesDistinct[i]);
-        $("#category").append(newOption);
-    }
-}
-
-function grabCategories() {
-    // Fetch categories from product table
-    $.get("/api/products", function (data) {
-        let categories = [];
-        for (i in data) {
-            categories.push(data[i].category);
-        }
-        console.log(categories);
-        categoriesDistinct = categories.filter(onlyUnique);
-        console.log(categoriesDistinct);
-        populateCategory();
-    });
-}
-grabCategories();
-
 $("#filterSearch").submit(function (event) {
     event.preventDefault();
     let category = $("#category").val().trim();
